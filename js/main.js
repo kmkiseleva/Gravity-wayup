25a5d475020bc54e422b96242fce1f8d3f7a473f
Ksenia Kiseleva <69579994+kmkiseleva@users.noreply.github.com>
2020-12-24 10:33:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
-$(function() {
-   $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:30,
-      nav:true,
-      dots: false,
-      navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
-      startPosition: 1,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:1
-          }
-      }
-  });
+$(function () {
+    $('.works-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        autoWidth:true,
+        dots:false,
+        navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        responsive:{
+         0:{
+          items:2
+         },
+         600:{
+          items:2
+         },
+         1000:{
+          items:2
+         }
+        }
+       });
+
+    $('.contributor-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        startPosition: 1,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
 });
